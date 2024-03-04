c289f5f53ce748001f85e29842371b909d71d433
Ricardo Yuri <127852978+ricardoyuuri@users.noreply.github.com>
2024-03-04 16:20:05 -0300

    Add files via upload

---
+++ src/main/java/com/ricardo/InsaneBeauty/InsaneBeautyApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.InsaneBeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsaneBeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsaneBeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/InsaneBeauty/InsaneBeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.InsaneBeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsaneBeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
